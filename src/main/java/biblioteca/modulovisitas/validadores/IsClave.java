f1753157b87e972c84c72ed6948d3ce77de9f991
matias <mutaclotos.mr@gmail.com>
2016-10-15 20:20:25 -0600

    formularios login normal y administrativo implementados

---
+++ src/main/java/biblioteca/modulovisitas/validadores/IsClave.java
@@ -0,0 +1,21 @@
+package biblioteca.modulovisitas.validadores;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.vaadin.data.Validator;
+
+public class IsClave implements Validator{
+
+	private static final long serialVersionUID = -1417033056435521050L;
+
+	@Override
+	public void validate(Object value) throws InvalidValueException {
+		Pattern p = Pattern.compile("([A-Za-z0-9]{6-10})?");
+		Matcher m = p.matcher(value.toString());
+		if(!m.matches()){
+			throw new InvalidValueException("No es una clave valida");
+		}
+	}
+	
+}
